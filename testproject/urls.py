60b34b3bcc34719ea80799dd2a6e9870c7db81eb
@MOSONG jrn <ropluke1@gmail.com>
2023-06-20 11:03:55 +0300

    first commit3

--- testproject/urls.py
+++ testproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from TestAPP import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.map.as_view(), name='map'),
 ]
