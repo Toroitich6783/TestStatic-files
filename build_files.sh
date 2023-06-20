3e96c62be0c0fd5908278bc70c6114b6e92891a4
@MOSONG jrn <ropluke1@gmail.com>
2023-06-20 12:35:25 +0300

    first commit8

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,16 @@
+#!/bin/bash
+
+# Activate the virtual environment (if you are using one)
+# source /path/to/venv/bin/activate
+
+# Install dependencies
 pip install -r requirements.txt
-python manage.py collectstatic
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run database migrations (if needed)
+# python manage.py migrate
+
+# Restart the web server (if applicable)
+# systemctl restart gunicorn.service
